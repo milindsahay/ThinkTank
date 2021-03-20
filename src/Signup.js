2c0fe86a6b8a626aeb14c5a4bd92702707ed4746
Milind Sahay <milind.sahay123@gmail.com>
2021-03-21 03:07:46 +0530

    sign in page initial styling

---
+++ src/Signup.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Signup.css'
+import {Form, Button, FormGroup} from "react-bootstrap";
+
+const Signup = () => {
+    return(
+        <div className="box effect">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="Password">
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="Button">
+                    <Button variant="primary" type="submit" block>
+                        Log In
+                    </Button>
+                </Form.Group>
+            </Form>
+            <Button variant="danger" type="submit" block>
+                Sign in with Google
+            </Button>
+            <hr/>
+            <Form>
+                <Form.Group controlId="Button">
+                    <Button variant="outline-dark" type="submit" block>
+                        Sign Up
+                    </Button>
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
+
+export default Signup
