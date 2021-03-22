2115b3f2e903bd9e315f42932b54dc678592f20f
Milind Sahay <milind.sahay123@gmail.com>
2021-03-22 18:07:03 +0530

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {Router} from "@reach/router";
 import Signup from "./Signup";
+import Home from "./home/Home";
+import {useState} from "react";
+
 function App() {
+    let [user, setUser] = useState(null)
     return (
         <Router>
             <Signup path="/" />
-            {/*<Home path="/home"/>*/}
+            <Home path="/home"/>
         </Router>
     );
 }
