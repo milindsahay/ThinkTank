efeb4b328b84e8574ed1852ace4760bb9375eecd
Milind Sahay <milind.sahay123@gmail.com>
2021-05-03 00:07:27 +0530

    aesthetics and add post refactor

--- src/home/Post.js
+++ src/home/Post.js
@@ -3,7 +3,8 @@ import {useEffect} from "react";
 import {db} from "../firebase";
 import {store} from "../redux_store";
 import {useSelector} from "react-redux";
-
+import CancelIcon from '@material-ui/icons/Cancel';
+import AlarmIcon from '@material-ui/icons/Alarm';
 
 const Post = () => {
     const posts = useSelector(state => state.posts)
@@ -29,6 +30,22 @@ const Post = () => {
             console.log(`error while deleting document ${e}`)
         }
     }
+
+    const getDateStringFromDate = (dateObj) => {
+        let dateArray = dateObj.toDate().toString().split(" ").slice(1,5)
+        let timeStamp = dateArray.pop().split(":")
+        let meridian = "AM"
+        // removing seconds
+        timeStamp.pop()
+        let hours  = parseInt(timeStamp[0])
+        if( hours > 12 ){
+            meridian = "PM"
+            hours-=12
+        }
+        timeStamp[0] = hours.toString();
+        let [month, date, year] = dateArray
+        return [date,month,year].join(" ") +" at " + timeStamp.join(":") + meridian
+    }
     return (
 
         <Container className='my-container'>
@@ -39,18 +56,13 @@ const Post = () => {
                         <Col md="auto"><img src={post.user && post.user.photoURL} className="post-img" alt="picture"/></Col>
                         <Col>
                             <Row><Col style={{'font-size':'25px', padding:'0', margin:'0', 'font-weight':'bold', 'line-height': '30px', 'margin-top': '1%', 'margin-bottom':'3px'}}>{post.user && post.user.displayName}</Col></Row>
-                            <Row><Col style={{ 'line-height': '80%', padding:'0', 'font-size': '12px' }}>{post && post.createdAt}</Col></Row>
+                            <Row><Col style={{ 'line-height': '80%', padding:'0', 'font-size': '12px', 'display': 'flex', 'align-items':'center'}}><AlarmIcon style={{'font-size':"12px", 'margin-right':'2px'}}/>{post && getDateStringFromDate(post.createdAt)}</Col></Row>
                         </Col>
+                        <Col><CancelIcon className="float-right" style={{color:'red', cursor: 'pointer'}} onClick={()=>deletePost(post.id)} /></Col>
                     </Row>
-                    <Row><Col><strong>{post.title}</strong></Col></Row>
                     <Row><Col>
                         <div>{post.body}</div>
                     </Col></Row>
-                    <Row className="bottom-bar">
-                        <Col className="pt-2">Posted by {post.user && post.user.displayName}</Col>
-                        <Col className="p-2">Time: {post.createdAt}</Col>
-                        <Col className="p-2"><Button variant="danger" onClick={()=>deletePost(post.id)}>Delete</Button></Col>
-                    </Row>
                 </Container>
                 )
             })}
